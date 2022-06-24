95033adc0d8a48305f99c85aac22f04ede5d0e6a
Michael <michaeltsiner@gmail.com>
2022-06-24 16:16:05 -0500

    create order screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -44,7 +44,7 @@ function reducer(state, action) {
       return { ...state, cart: { ...state.cart, cartItems } }
     }
     case "CART_CLEAR":
-      return { ...state, cart: { ...state.cart, cartItems } }
+      return { ...state, cart: { ...state.cart, cartItems: [] } }
     case "USER_SIGNIN":
       return { ...state, userInfo: action.payload }
     case "USER_SIGNOUT":
