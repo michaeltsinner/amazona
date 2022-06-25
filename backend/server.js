d36f791cf2577028b4fb18bb9d0db78bcd04b93c
michaeltsinner <michaeltsinner@gmail.com>
2022-06-25 18:10:47 -0500

    implement PayPal payments

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb') 
+})
+
 app.use(cors())
 
 app.use("/api/seed", seedRouter)
